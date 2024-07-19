ef2e19a875430edc8961a566f39aa69e1a049e7a
webart <rudi.darmawan@gmail.com>
2024-07-19 11:00:39 +0700

    update dashoboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
-// import jwtDecode from "jwt-decode";
 import {jwtDecode as jwtDecode} from 'jwt-decode';
 import { useHistory } from 'react-router-dom';
  
